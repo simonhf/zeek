8f7b5a3d0738756cd06cf3e15a4340348006c26c
Tim Wojtulewicz <tim@corelight.com>
2019-11-13 17:50:21 +0000

    Add epoll() support

--- src/iosource/Manager.cc
+++ src/iosource/Manager.cc
@@ -156,20 +156,129 @@ std::set<IOSource*> Manager::FindReadySources(bool& timer_expired)
 
 void Manager::InitQueue()
 	{
-	event_queue = epoll_create(1);
+	event_queue = epoll_create1(EPOLL_CLOEXEC);
+	if ( event_queue == -1 )
+		reporter->FatalError("Failed to open epoll() file descriptor: %s", strerror(errno));
+
+	timerfd = timerfd_create(CLOCK_MONOTONIC, 0);
+	if ( timerfd == -1 )
+		reporter->FatalError("Failed to initialize timerfd: %s", strerror(errno));
+
+	epoll_event event;
+	memset(&event, 0, sizeof(epoll_event));
+	event.events = EPOLLIN;
+
+	if ( epoll_ctl(event_queue, EPOLL_CTL_ADD, timerfd, &event) != -1 )
+		{
+		DBG_LOG(DBG_MAINLOOP, "Added fd %d from Timerfd", timerfd);
+		events.push_back({});
+		}
 	}
 
 void Manager::RegisterFd(int fd, IOSource* src)
 	{
+	epoll_event event;
+	memset(&event, 0, sizeof(epoll_event));
+	event.events = EPOLLIN;
+	event.data.fd = fd;
+
+	int ret = epoll_ctl(event_queue, EPOLL_CTL_ADD, fd, &event);
+	if ( ret != -1 )
+		{
+		DBG_LOG(DBG_MAINLOOP, "Registered fd %d from %s", fd, src->Tag());
+		events.push_back({});
+		fd_map[fd] = src;
+		Wakeup("RegisterFd");
+		}
+	else
+		{
+		DBG_LOG(DBG_MAINLOOP, "Failed to register fd %d from %s: %s", fd, src->Tag(), strerror(errno));
+		}
 	}
 
 void Manager::UnregisterFd(int fd)
 	{
+	if ( fd_map.find(fd) != fd_map.end() )
+		{
+		int ret = epoll_ctl(event_queue, EPOLL_CTL_DEL, fd, NULL);
+		if ( ret != -1 )
+			DBG_LOG(DBG_MAINLOOP, "Unregistered fd %d", fd);
+
+		events.pop_back();
+		fd_map.erase(fd);
+
+		Wakeup("UnregisterFd");
+		}
 	}
 
 bool Manager::Poll(std::set<IOSource*>& ready, double timeout, IOSource* timeout_src)
 	{
 	bool timer_expired = false;
+
+	// TODO: double comparisons are fraught with danger
+	if ( timeout != 0 )
+		{
+		struct itimerspec new_timeout;
+		memset(&new_timeout, 0, sizeof(new_timeout));
+		ConvertTimeout(timeout, new_timeout.it_value);
+		timerfd_settime(timerfd, 0, &new_timeout, NULL);
+		}
+
+	// Because of the way timerfd works, you can't just set it to a zero
+	// timeout. That deactivates the timer. That means if the timeout
+	// passed in was zero, we need to pass that zero down to poll().
+	// Otherwise, set it to -1 and let timerfd do its thing.
+	int ret = epoll_wait(event_queue, events.data(), events.size(), timeout == 0 ? 0 : -1);
+	if ( ret == -1 )
+		{
+		// TODO: something better here than just a perror
+		if ( errno != EINTR )
+			perror("poll error");
+		}
+	else if ( ret == 0 )
+		{
+		if ( timeout_src )
+			ready.insert(timeout_src);
+		else
+			timer_expired = true;
+		}
+	else
+		{
+		DBG_LOG(DBG_MAINLOOP, "epoll says %d things are ready", ret);
+		for ( int i = 0; i < ret; i++ )
+			{
+			DBG_LOG(DBG_MAINLOOP, "%d is ready, status is %d", events[i].data.fd, events[i].events);
+			// This theoretically shouldn't be possible, but best to check
+			// anyways. If one of the ready file descriptors is the timer
+			// then we only want to retrieve that one timer source and
+			// ignore the rest. They'll still be ready the next time around.
+			// TODO: does this make sense?
+			if ( events[i].data.fd == timerfd && events[i].events == EPOLLIN )
+				{
+				uint64_t elapsed;
+				read(timerfd, &elapsed, 8);
+
+				ready.clear();
+				if ( timeout_src )
+					ready.insert(timeout_src);
+				else
+					timer_expired = true;
+				break;
+				}
+			else
+				{
+				auto entry = fd_map.find(events[i].data.fd);
+				if ( entry != fd_map.end() )
+					{
+					if ( events[i].events == EPOLLIN )
+						ready.insert(entry->second);
+					else if ( events[i].events == EPOLLERR || events[i].events == EPOLLHUP )
+						printf("Source %s returned an error from poll (0x%x)\n",
+							entry->second->Tag(), events[i].events);
+					}
+				}
+			}
+		}
 
 	return timer_expired;
 	}
